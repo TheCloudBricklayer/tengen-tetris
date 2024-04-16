fd4d637b384c847afb41e3b42550bd0b3a6eb45e
TheCloudBrickLayer <item.eduardoamaro@gmail.com>
2024-04-16 12:55:32 -0600

    fix: variable error

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     app_host    = os.getenv('APP_HOST', '0.0.0.0')
-    app_port    = os.getenv('APP_PORT', '8080')
+    app_port    = os.getenv('APP_PORT', 8080)
     debug       = os.getenv('DEBUG', False)
